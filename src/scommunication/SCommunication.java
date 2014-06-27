9c722e1ce00d9c50b41a171ac996a5fe5562e7ae
plachste <plachy.s@seznam.cz>
2014-06-27 20:45:05 +0200

    Revert "Revert "Revert "minor changes"""
    This reverts commit af8a9324df1cee3141a954fb8a0732c1aaf02556.

--- src/scommunication/SCommunication.java
+++ src/scommunication/SCommunication.java
@@ -4,15 +4,6 @@
  */
 package scommunication;
 
-import communication.backend.client.SCommunicationClient;
-import communication.backend.server.SCommunicationClientHandler;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.Reader;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.Scanner;
-
 /**
  *
  * @author plach_000
@@ -22,18 +13,7 @@ public class SCommunication {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws UnknownHostException, IOException {
-        String ip = args[1];
-        int port = Integer.parseInt(args[2]);
-        PrintAction action;
-        new BufferedReader()
-        if("-s".equals(args[0])) {
-            SCommunicationClientHandler handler = new SCommunicationClientHandler(new Socket(ip, port));
-            while(true) {
-                
-            }
-        } else if("-c".equals(args[0])) {
-            
-        }
+    public static void main(String[] args) {
+        
     }
 }
