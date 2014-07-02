b8b4017b561740934cd3f5c9251dfc2ec0729ff8
plachste <plachy.s@seznam.cz>
2014-07-02 20:25:49 +0200

    added SNullAction

---
+++ src/communication/backend/utilities/SNullAction.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package communication.backend.utilities;
+
+import communication.frontend.utilities.Performable;
+
+/**
+ *
+ * @author plach_000
+ */
+public class SNullAction implements Performable{
+
+    @Override
+    public void perform() {
+    }
+    
+}
